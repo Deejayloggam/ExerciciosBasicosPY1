7e6968987e643c598341ed61bb67f2e3802ff320
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 12:56:48 -0300

    Create Conversor_m_cm_mm.py

---
+++ Conversor_m_cm_mm.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia um valor em metros e exiba convertido em centímetros e milímetros
+metro = float(input('Digite o valor em metros para obter sua conversão\n'))
+print(f'O valor em centímetros é {metro*100} cm\nO valor em milímetros é {metro*1000} mm')
