db4ddc8434aa9be27cf61c21a371bf2c3b3ac7ed
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 12:49:33 -0300

    Create Dobro_triplo_raiz2.py

---
+++ Dobro_triplo_raiz2.py
@@ -0,0 +1,3 @@
+# crie um algoritmo que leia um número e dê o seu dobro, triplo e raiz quadrada
+a = int(input('Digite um número para obter o seu dobro, triplo e raiz quadrada\n'))
+print(f'Seu dobro é {a*2}\nSeu triplo é {a*3}\nSua raiz quadrada é {a**1/2}')
