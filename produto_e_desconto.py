23876ad05a38991183dd6d329d769e5e2b332cfc
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 13:18:50 -0300

    Create produto_e_desconto.py

---
+++ produto_e_desconto.py
@@ -0,0 +1,3 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
+a = float(input('Digite o preço do produto '))
+print(f'O valor do produto com 5% de desconto é {a-a*0.05} ')
