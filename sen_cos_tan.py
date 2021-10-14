bf236c70172d2cefb0fa78c46bc73812ded52b72
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 18:45:45 -0300

    Create sen_cos_tan.py

---
+++ sen_cos_tan.py
@@ -0,0 +1,4 @@
+from math import cos, tan, sin
+# faça um programa que leia um ângulo qualquer e mostre na tela seu seno, cosseno e tangente
+a = float(input('Digite um ângulo: '))
+print(f'Cosseno = {cos(a)}\nTangente = {tan(a)}\nseno = {sin(a)}')
