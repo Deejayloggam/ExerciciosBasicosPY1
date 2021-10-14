d57c5d3884eda7a2512c66bfb38f0838b3ddd446
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 13:09:39 -0300

    Create dolar14102021.py

---
+++ dolar14102021.py
@@ -0,0 +1,4 @@
+# crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos doláres ela pode comprar
+a = float(input('Quanto dinheiro você tem?'))
+print(f'Você pode comprar {a/5.51} dólar(es) hoje, 14/10/2021')
+# Muito provavelmente vou olhar esse trabalho no futuro e pensar "como o dólar estava barato"...
