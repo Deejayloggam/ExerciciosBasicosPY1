c188d3019c3b453fcd95c2d9893edff06979939d
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 12:52:35 -0300

    Create Media_2_numeros.py

---
+++ Media_2_numeros.py
@@ -0,0 +1,4 @@
+#  Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
+a = float(input('Digite a primeira nota de seu aluno \n'))
+b = float(input('Digite a segunda nota de seu aluno \n'))
+print((a+b)/2)
