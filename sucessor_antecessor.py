b330dd0a503d974a62102325c017220a1dbf23f9
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 12:45:21 -0300

    Create sucessor_antecessor.py

---
+++ sucessor_antecessor.py
@@ -0,0 +1,3 @@
+# faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+a = int(input('Digite um número inteiro \n'))
+print(f'O seu sucessor é {a+1}\nO seu antecessor é {a-1}')
