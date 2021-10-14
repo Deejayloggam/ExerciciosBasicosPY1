b576d3c4a2495b1bc55f94bd14cb34d8f8c9a94b
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-14 18:35:39 -0300

    Create trunc.py

---
+++ trunc.py
@@ -0,0 +1,4 @@
+from math import trunc
+# crie um programa que leia um número real qualquer e mostre na tela a sua porção inteira
+a = float(input('Digite um número\n'))
+print(f'O número truncado de {a} é {trunc(a)}')
