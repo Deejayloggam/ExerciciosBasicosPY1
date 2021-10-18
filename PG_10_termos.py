0f740b4e988fe9de700aa77066f1d5d95c4cd929
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-18 18:21:20 -0300

    Update and rename PA_10_termos.py to PG_10_termos.py

--- PG_10_termos.py
+++ PG_10_termos.py
@@ -1,4 +1,4 @@
-# desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos.
+# desenvolva um programa que leia o primeiro termo e a razão de uma PG. No final, mostre os 10 primeiros termos.
 a = int(input('Digite o primeiro termo de uma PA: '))
 b = float(input('Digite a razão dessa PA: '))
 for c in range(1, 11):
