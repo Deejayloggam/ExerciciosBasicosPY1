884558e2ff344c8a4bae5f7f91a2a4030e21fa4c
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-18 16:35:33 -0300

    Create for_and_sleep.py

---
+++ for_and_sleep.py
@@ -0,0 +1,5 @@
+from time import sleep  # faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificio, indo de 10 até 0
+for c in range(1, 11):  # com uma pausa de 1 segundo entre eles.
+    print(c)
+    sleep(1)
+print('boooom')
