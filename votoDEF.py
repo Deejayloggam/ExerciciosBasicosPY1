0c9e9850aa024469d133c4dbc025872a0523a81d
Luis Gustavo <59546884+Deejayloggam@users.noreply.github.com>
2021-10-21 22:47:42 -0300

    Create votoDEF.py

---
+++ votoDEF.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+# crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa
+# retornando o valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL OU OBRIGATÓRIO nas eleições
+
+
+def voto(a):
+    if datetime.now().year - a > 18:
+        return 'OBRIGATÓRIO'
+    elif 16 <= datetime.now().year - a < 18:
+        return 'OPCIONAL'
+    else:
+        return 'NEGADO'
+
+
+b = voto(int(input('SEU ANO DE NASCIMENTO: ')))
+print(f'SEU VOTO É: {b}')
